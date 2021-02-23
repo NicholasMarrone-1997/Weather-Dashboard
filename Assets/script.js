36acb7565379e3cceda5c860b82684bbc4257fee
Nicholas Marrone <nmarrone1997@gmail.com>
2021-02-23 17:52:22 -0500

    Retrieved the city and current date, trying to figure out how to append date to city

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,3 @@
-//var currentDate = moment();
-//$("#city_name").text(currentDate.format("MMM Do, YYYY"));
 // 06bf71c585fc92aee380df18e65dac7d
 
 var cityUrl = 'https://api.openweathermap.org/data/2.5/weather?q=charlotte&appid=06bf71c585fc92aee380df18e65dac7d&';
@@ -9,18 +7,14 @@ fetch(cityUrl)
         return response.json();
     })
     .then(function (data) {
+        //Display City Name
         console.log(data);
         var weatherEl = document.querySelector('#city_name');
         console.log(weatherEl);
-        weatherEl.textContent = data.main.name;
+        weatherEl.textContent = data.name;
+
+        var d = new Date();
+        var strDate = (d.getMonth() + 1) + "/" + d.getDate() + "/" + d.getFullYear();
+        console.log(strDate);
         
     })
-
-
-
-
-
-
-
-
-
