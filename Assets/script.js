1f66a3fec14b8354c25b35bea7ca60ae0506326e
Nicholas Marrone <nmarrone1997@gmail.com>
2021-02-24 13:40:19 -0500

    Changes to JS

--- Assets/script.js
+++ Assets/script.js
@@ -22,34 +22,34 @@ button.addEventListener('click', function (e) {
     var city = $('#form1').val();
     var userCity = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=06bf71c585fc92aee380df18e65dac7d`;
     handleFetch(userCity);
+    getData();
 })
 
-fetch(userCity)
-  .then(function (response) {
-    return response.json();
-})
-.then(function (data) {
-  //Display City Name
-console.log(data);
-var weatherEl = document.querySelector('#city_name');
-weatherEl.textContent = data.name;
+//fetch(userCity)
+// .then(function (response) {
+// return response.json();
+//})
+//.then(function (data) {
+//Display City Name
+//console.log(data);
+function getData() {
+    var weatherEl = document.querySelector('#city_name');
+    weatherEl.textContent = data.name;
 
-var d = new Date();
-var strDate = "(" + (d.getMonth() + 1) + "/" + d.getDate() + "/" + d.getFullYear() + ")";
-weatherEl.append(" ");
-weatherEl.append(strDate);
+    var d = new Date();
+    var strDate = "(" + (d.getMonth() + 1) + "/" + d.getDate() + "/" + d.getFullYear() + ")";
+    weatherEl.append(" ");
+    weatherEl.append(strDate);
 
-var tempEl = document.querySelector('#temp');
-tempEl.append(data.main.temp);
+    var tempEl = document.querySelector('#temp');
+    tempEl.append(data.main.temp);
 
-var humEl = document.querySelector('#hum');
-humEl.append(data.main.humidity);
+    var humEl = document.querySelector('#hum');
+    humEl.append(data.main.humidity);
 
-var windEl = document.querySelector('#wind');
-windEl.append(data.wind.speed);
-
-
- })
+    var windEl = document.querySelector('#wind');
+    windEl.append(data.wind.speed);
+}
 
 fetch(uvIndex)
     .then(function (response) {
