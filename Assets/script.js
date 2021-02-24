ff8b74d33e881f318e02fa1ce1f6c053daf7ebee
Nicholas Marrone <nmarrone1997@gmail.com>
2021-02-23 21:53:09 -0500

    Trying to figure out why template literal is not working

--- Assets/script.js
+++ Assets/script.js
@@ -4,13 +4,15 @@ var cityUrl = 'https://api.openweathermap.org/data/2.5/weather?q=charlotte&appid
 var uvIndex = 'http://api.openweathermap.org/data/2.5/uvi?lat=35.2271&lon=-80.8431&appid=06bf71c585fc92aee380df18e65dac7d';
 var fiveDay = 'http://api.openweathermap.org/data/2.5/forecast?q=charlotte&appid=06bf71c585fc92aee380df18e65dac7d';
 var city = "";
-var userCity = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=06bf71c585fc92aee380df18e65dac7d`;
+//var userCity = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=06bf71c585fc92aee380df18e65dac7d`;
+//console.log(userCity);
 
 var button = document.querySelector('#button');
 
 button.addEventListener('click', function(){
     city = $('#form1').val();
-    console.log(city);
+    userCity = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=06bf71c585fc92aee380df18e65dac7d`;
+    console.log(userCity);
 
 })
 
@@ -22,26 +24,21 @@ fetch(userCity)
         //Display City Name
         console.log(data);
         var weatherEl = document.querySelector('#city_name');
-        console.log(weatherEl);
         weatherEl.textContent = data.name;
 
         var d = new Date();
         var strDate = "(" + (d.getMonth() + 1) + "/" + d.getDate() + "/" + d.getFullYear() + ")";
-        console.log(strDate);
         weatherEl.append(" ");
         weatherEl.append(strDate);
 
         var tempEl = document.querySelector('#temp');
         tempEl.append(data.main.temp);
-        console.log(tempEl);
 
         var humEl = document.querySelector('#hum');
         humEl.append(data.main.humidity);
-        console.log(humEl);
 
         var windEl = document.querySelector('#wind');
         windEl.append(data.wind.speed);
-        console.log(windEl);
 
         
     })
