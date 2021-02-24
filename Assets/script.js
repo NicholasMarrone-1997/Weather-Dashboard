7fe781093cd1e7808b4f9e64fc91cc60b8b39e10
Nicholas Marrone <nmarrone1997@gmail.com>
2021-02-23 19:02:01 -0500

    Trying to get UV object to implement in webpage

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,7 @@
 // 06bf71c585fc92aee380df18e65dac7d
 
 var cityUrl = 'https://api.openweathermap.org/data/2.5/weather?q=charlotte&appid=06bf71c585fc92aee380df18e65dac7d&';
+var uvIndex = 'http://api.openweathermap.org/data/2.5/uvi?lat=35.2271&lon=-80.8431&appid=06bf71c585fc92aee380df18e65dac7d';
 
 fetch(cityUrl)
     .then(function (response) {
@@ -20,6 +21,25 @@ fetch(cityUrl)
         weatherEl.append(strDate);
 
         var tempEl = document.querySelector('#temp');
+        tempEl.append(data.main.temp);
         console.log(tempEl);
+
+        var humEl = document.querySelector('#hum');
+        humEl.append(data.main.humidity);
+        console.log(humEl);
+
+        var windEl = document.querySelector('#wind');
+        windEl.append(data.wind.speed);
+        console.log(windEl);
+
         
+    })
+
+fetch(uvIndex)
+    .then(function (response) {
+        return response.json
+    })
+    .then(function (data) {
+        console.log(data);
+
     })
